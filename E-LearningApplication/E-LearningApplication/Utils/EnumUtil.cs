6db15346ea27bdfaa598d19f4d2f3ff76f98fbe3
Andra-Ristei <ristei.andra@gmail.com>
2016-01-10 00:31:58 +0200

    fixed some bugs, added enums + changed DB

---
+++ E-LearningApplication/E-LearningApplication/Utils/EnumUtil.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_LearningApplication.Utils {
+    public enum RolesEnum { Admin, Prof, Student }
+    public enum SubmissionTypeEnum { File, Text }
+    public enum GroupTypeEnum { Private, Public }
+}
